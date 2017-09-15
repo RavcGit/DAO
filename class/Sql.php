8372ff9c6ab81e6d0925ad81251f21b62598aa50
Ravc <rodrigo@ravc.com.br>
2017-09-14 21:35:02 -0300

    criado controles de acessos para a classe usuario

--- class/Sql.php
+++ class/Sql.php
@@ -6,29 +6,29 @@ class Sql extends PDO {
 
 	public function __construct(){
 
-		$this->conn = new PDO("mysql:host=localhost;dbname", "root", "");
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 
 	}
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 
 		foreach ($parameters as $key => $value) {
 			
-			$$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 
 		}
 
 	}
 
-	private function setParam($statment, $key, $value){
+	private function setParam($statement, $key, $value){
 
-		$statment->bindParam($key, $value);
+		$statement->bindParam($key, $value);
 
 	}
 
 	public function query($rawQuery, $params = array()){
 
-		$stmt = $this->conn->prepare($RawQuery);
+		$stmt = $this->conn->prepare($rawQuery);
 
 		$this->setParams($stmt, $params);
 
--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php 
 
+require_once("config.php");
+
+//Carregar um usuario
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
+
+// Lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carregar usuarios pelo login
+//$search = Usuario::search("us");
+//echo json_encode($search);
+
+//carregar usuario usando login e senha
+//$usuario = new Usuario();
+//$usuario->login("master", "master");
+//echo $usuario;
 
 
  ?>
